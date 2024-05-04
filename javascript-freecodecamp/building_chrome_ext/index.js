1013bb53c90213f11c1a8d3e2cae1ac5883beec9
Gdija <khadijaghadi00@gmail.com>
2024-05-04 14:52:22 +0100

    add files

--- javascript-freecodecamp/building_chrome_ext/index.js
+++ javascript-freecodecamp/building_chrome_ext/index.js
@@ -3,42 +3,58 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
-// localStorage.clear()
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-
+const tabBtn = document.getElementById("tab-btn")
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
-  renderLeads()
-  myLeads = []
-  renderLeads()
+  render(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    //clear out the input field
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
-
-    console.log( localStorage.getItem("myLeads") )
-
-})
-
-// 1. Create a variable, listItems, to hold all the HTML for the list items
-// Assign it to an empty string to begin with
-function renderLeads(){
+function render(leads){
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
         <li>
-          <a target='_blank' href='${myLeads[i]}'> 
-            ${myLeads[i]}
+          <a target='_blank' href='${leads[i]}'> 
+            ${leads[i]}
           </a>
         </li>
         `
     }
     ulEl.innerHTML = listItems
 }
+
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function(){
+  // Save the url instead of logging it out
+  myLeads.push(tabs[0].url)
+    //clear out the input field
+    // inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
+
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    //clear out the input field
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
+
+// 1. Create a variable, listItems, to hold all the HTML for the list items
+// Assign it to an empty string to begin with
+
 
 
