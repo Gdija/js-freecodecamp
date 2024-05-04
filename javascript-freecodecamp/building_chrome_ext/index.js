f57282ab8f26ef594bc3474701ad8dbd96ef7efe
Gdija <khadijaghadi00@gmail.com>
2024-05-04 10:54:24 +0100

    add files

--- javascript-freecodecamp/building_chrome_ext/index.js
+++ javascript-freecodecamp/building_chrome_ext/index.js
@@ -5,6 +5,8 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    //clear out the input field
+    inputEl.value = ""
     renderLeads()
 })
 
@@ -13,8 +15,15 @@ inputBtn.addEventListener("click", function() {
 function renderLeads(){
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li> "
+        listItems += `
+        <li>
+          <a target='_blank' href='${myLeads[i]}'> 
+            ${myLeads[i]}
+          </a>
+        </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
+
 
