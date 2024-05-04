f30cca8cd7176d83bc85b70e9ae149cb35242ead
Gdija <khadijaghadi00@gmail.com>
2024-05-04 12:53:51 +0100

    localstorage

--- javascript-freecodecamp/building_chrome_ext/index.js
+++ javascript-freecodecamp/building_chrome_ext/index.js
@@ -2,12 +2,27 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+// localStorage.clear()
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+  myLeads = []
+  renderLeads()
+}
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     //clear out the input field
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+
+    console.log( localStorage.getItem("myLeads") )
+
 })
 
 // 1. Create a variable, listItems, to hold all the HTML for the list items
